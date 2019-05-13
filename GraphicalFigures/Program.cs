feb1ff1daee797579096f20841824f8d0c0c1a33
Evaapostol <eugeniaapostol@hotmail.gr>
2019-05-13 05:38:19 +0300

    Add project files.

---
+++ GraphicalFigures/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GraphicalFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GraphicalRepresentation(7);
+            Console.WriteLine();
+            GraphicalRepresentation(4);
+            Console.WriteLine();
+            GraphicalRepresentation(3);
+        }
+
+        static void GraphicalRepresentation(int figure)
+        {
+            for (int i= 0; i < figure; i++)
+                Console.Write('*');
+        }
+    }
+}
